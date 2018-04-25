8629638f44a894277934141d0b30ad31f83210aa
Wenfeng <Irlyue@outlook.com>
2018-04-25 22:48:51 +0800

    changes

--- models/fcn32.py
+++ models/fcn32.py
@@ -10,6 +10,9 @@ class FCN32(FCN):
     def __init__(self, backbone):
         super().__init__(backbone, 'fcn32')
 
+    def get_solver(self):
+        return tf.train.AdamOptimizer(learning_rate=self.params.lr)
+
     def inference(self):
         params = self.params
         endpoints = OrderedDict()
@@ -25,17 +28,17 @@ class FCN32(FCN):
 
         output = tf.argmax(conv6, axis=-1, name='output')
 
-        # up32_conv = slim.conv2d_transpose(conv6,
-        #                                   num_outputs=params.n_classes+1,
-        #                                   kernel_size=params.backbone_stride*2,
-        #                                   stride=params.backbone_stride,
-        #                                   scope='up_conv%d' % params.backbone_stride,
-        #                                   activation_fn=None)
-        up32_conv = tf.image.resize_images(conv6, params.image_size)
+        stride = 16
+        with tf.variable_scope('up_conv%d' % stride):
+            logits_shape = tf.shape(conv6)
+            up_logits_shape = [logits_shape[0], logits_shape[1]*stride, logits_shape[2]*stride, logits_shape[3]]
+            up_filter = tf.constant(mu.bilinear_upsample_weights(stride, params.n_classes+1), dtype=tf.float32)
+            up_logits = tf.nn.conv2d_transpose(conv6, up_filter, output_shape=up_logits_shape,
+                                               strides=[1, stride, stride, 1])
 
-        up_output = tf.argmax(up32_conv, axis=-1, name='up_output')
+        up_output = tf.argmax(up_logits, axis=-1, name='up_output')
         endpoints.update(output=output, up_output=up_output)
-        return up32_conv, endpoints, backbone_hooks
+        return up_logits, endpoints, backbone_hooks
 
 
 class VGGFCN32(FCN):

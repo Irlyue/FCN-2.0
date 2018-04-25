3031a68b262ab30c4c22f203bf45f5fdbed005a8
Wenfeng <Irlyue@outlook.com>
2018-04-25 14:49:41 +0800

    huge reformatting

--- models/fcn32.py
+++ models/fcn32.py
@@ -3,72 +3,12 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
 from .model import FCN
-from .optimizer import OptimizerWrapper
-from .tf_hooks import EvalBestHook, EvalHook
 from collections import OrderedDict
 
 
 class FCN32(FCN):
     def __init__(self, backbone):
-        super().__init__('fcn32')
-        self.backbone = backbone
-
-    def __call__(self, features, labels, mode, params):
-        with tf.device('/GPU:{}'.format(params.gpu_id)):
-            self.on_call(features, labels, mode, params)
-            logits, endpoints, backbone_hooks = self.inference()
-
-            if self.predict_mode():
-                predictions = {
-                    'output': endpoints['output'],
-                    'up_output': endpoints['up_output']
-                }
-                return tf.estimator.EstimatorSpec(mode, predictions=predictions)
-
-            reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES), name='reg_loss')
-            data_loss = mu.sparse_softmax_cross_entropy(labels=labels['mask'], logits=logits)
-            loss = tf.add_n([reg_loss, data_loss], name='total_loss')
-            ##############################
-            #          metrics           #
-            ##############################
-            with tf.name_scope('metrics'):
-                with tf.name_scope('accuracy'):
-                    accuracy = tf.metrics.accuracy(labels=labels['mask'], predictions=endpoints['up_output'])
-                mIoU = mu.mean_iou(labels=labels['mask'], predictions=endpoints['up_output'], num_classes=params.n_classes+1)
-
-            if self.eval_mode():
-                metrics = {
-                    'accuracy': accuracy,
-                    'mIoU': mIoU
-                }
-                eval_hooks = [EvalHook(), EvalBestHook(metrics['mIoU'][0], model_dir=params.model_dir)]
-                return tf.estimator.EstimatorSpec(mode, loss=data_loss, eval_metric_ops=metrics,
-                                                  evaluation_hooks=eval_hooks)
-
-            if self.train_mode():
-                ##############################
-                #          summary           #
-                ##############################
-                tf.summary.scalar('loss/data_loss', data_loss)
-                tf.summary.scalar('loss/reg_loss', reg_loss)
-                tf.summary.scalar('lr', params.lr)
-                tf.summary.scalar('metrics/accuracy', accuracy[1])
-                # tf.summary.scalar('metrics/mIoU', mIoU[1])
-
-                mu.add_moving_average(beta=0.99, scope='variable_moving_average')
-                solver = OptimizerWrapper(self.params.solver,
-                                          params={
-                                              self.backbone.name: self.params.lr*self.params.lrp,
-                                              'conv6': self.params.lr,
-                                              'up_conv32': self.params.lr,
-                                          })
-                apply_gradient_op = solver.minimize(loss, global_step=self.global_step)
-                tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, apply_gradient_op)
-
-                with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-                    train_op = tf.no_op(name='train_op')
-                train_hooks = backbone_hooks
-                return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, training_hooks=train_hooks)
+        super().__init__(backbone, 'fcn32')
 
     def inference(self):
         params = self.params
@@ -96,4 +36,29 @@ class FCN32(FCN):
         up_output = tf.argmax(up32_conv, axis=-1, name='up_output')
         endpoints.update(output=output, up_output=up_output)
         return up32_conv, endpoints, backbone_hooks
+
+
+class VGGFCN32(FCN):
+    def __init__(self, backbone):
+        super().__init__(backbone, name='VGGFCN32')
+
+    def __call__(self, features, labels, mode, params):
+        super().__call__(features, labels, mode, params)
+
+    def get_solver(self):
+        return tf.train.AdamOptimizer(learning_rate=self.params.lr)
+
+    def inference(self):
+        params = self.params
+        endpoints = OrderedDict()
+        logits, backbone_hooks = self.backbone(self.features, self.mode, self.params)
+
+        stride = 32
+        with tf.variable_scope('up_conv%d' % stride):
+            logits_shape = tf.shape(logits)
+            up_logits_shape = [logits_shape[0], logits_shape[1]*stride, logits_shape[2]*stride, logits_shape[3]]
+            up_filter = tf.constant(mu.bilinear_upsample_weights(stride, params.n_classes+1), dtype=tf.float32)
+            up_logits = tf.nn.conv2d_transpose(logits, up_filter, output_shape=up_logits_shape,
+                                               strides=[1, stride, stride, 1])
+        return up_logits, endpoints, backbone_hooks
 
--- models/model.py
+++ models/model.py
@@ -2,23 +2,81 @@ import my_utils as mu
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-from .tf_hooks import RestoreHook
+from .optimizer import OptimizerWrapper
+from .tf_hooks import EvalBestHook, EvalHook, RestoreHook
 from tensorflow.contrib.framework import assign_from_checkpoint_fn
 
 
 class FCN:
-    def __init__(self, name='fcn'):
+    def __init__(self, backbone, name='fcn'):
+        self.backbone = backbone
         self.name = name
 
     def __call__(self, features, labels, mode, params):
         self.on_call(features, labels, mode, params)
-        self.inference()
+        with tf.device('/GPU:{}'.format(params.gpu_id)):
+            logits, endpoints, backbone_hooks = self.inference()
+            if self.predict_mode():
+                predictions = {
+                    'output': endpoints['output'],
+                    'up_output': endpoints['up_output']
+                }
+                return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+            reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES), name='reg_loss')
+            data_loss = mu.sparse_softmax_cross_entropy(labels=labels['mask'], logits=logits)
+            loss = tf.add_n([reg_loss, data_loss], name='total_loss')
+            ##############################
+            #          metrics           #
+            ##############################
+            with tf.name_scope('metrics'):
+                with tf.name_scope('accuracy'):
+                    accuracy = tf.metrics.accuracy(labels=labels['mask'], predictions=endpoints['up_output'])
+                mIoU = mu.mean_iou(labels=labels['mask'], predictions=endpoints['up_output'], num_classes=params.n_classes+1)
+
+            if self.eval_mode():
+                metrics = {
+                    'accuracy': accuracy,
+                    'mIoU': mIoU
+                }
+                eval_hooks = [EvalHook(), EvalBestHook(metrics['mIoU'][0], model_dir=params.model_dir)]
+                return tf.estimator.EstimatorSpec(mode, loss=data_loss, eval_metric_ops=metrics,
+                                                  evaluation_hooks=eval_hooks)
+
+            if self.train_mode():
+                ##############################
+                #          summary           #
+                ##############################
+                tf.summary.scalar('loss/data_loss', data_loss)
+                tf.summary.scalar('loss/reg_loss', reg_loss)
+                tf.summary.scalar('lr', params.lr)
+                tf.summary.scalar('metrics/accuracy', accuracy[1])
+                # tf.summary.scalar('metrics/mIoU', mIoU[1])
+
+                mu.add_moving_average(beta=0.99, scope='variable_moving_average')
+                solver = self.get_solver()
+                apply_gradient_op = solver.minimize(loss, global_step=self.global_step)
+                tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, apply_gradient_op)
+
+                with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+                    train_op = tf.no_op(name='train_op')
+                train_hooks = backbone_hooks
+                return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, training_hooks=train_hooks)
 
     def on_call(self, features, labels, mode, params):
         self.features = features
         self.labels = labels
         self.mode = mode
         self.params = params
+
+    def get_solver(self):
+        solver = OptimizerWrapper(self.params.solver,
+                                  params={
+                                      self.backbone.name: self.params.lr*self.params.lrp,
+                                      'conv6': self.params.lr,
+                                      'up_conv32': self.params.lr,
+                                  })
+        return solver
 
     def train_mode(self):
         return self.mode == tf.estimator.ModeKeys.TRAIN
@@ -72,6 +130,54 @@ class InputFunction:
             return batch_x, batch_y
 
 
+class SegInputFunction:
+    def __init__(self, tfrecord_path, training, batch_size=1, prep_fn=None, n_epochs=1):
+        self.tfrecord_path = tfrecord_path
+        self.training = training
+        self.batch_size = batch_size
+        self.prep_fn = prep_fn
+        self.n_epochs = n_epochs
+
+    def __call__(self, *args, **kwargs):
+        def _parse_function(example_proto):
+            features = {
+                "height": tf.FixedLenFeature([], tf.int64),
+                "width": tf.FixedLenFeature([], tf.int64),
+                "image_raw": tf.FixedLenFeature([], tf.string),
+                "mask_raw": tf.FixedLenFeature([], tf.string)
+            }
+            parsed = tf.parse_single_example(example_proto, features)
+
+            image = tf.decode_raw(parsed['image_raw'], tf.uint8)
+            mask = tf.decode_raw(parsed['mask_raw'], tf.uint8)
+            height = tf.cast(parsed['height'], tf.int32)
+            width = tf.cast(parsed['width'], tf.int32)
+            image = tf.reshape(image, shape=(height, width, 3))
+            mask = tf.reshape(mask, shape=(height, width, 1))
+            return image, mask
+
+        def to_dtype(image, label):
+            image = tf.cast(image, tf.float32)
+            label = tf.cast(label, tf.int32)
+            return image, label
+
+        with tf.device('/CPU:0'):
+            data = tf.data.TFRecordDataset([self.tfrecord_path])
+            data = data.map(_parse_function)
+            data = data.map(to_dtype)
+            if self.prep_fn:
+                data = data.map(lambda image, label: self.prep_fn(image, label, training=self.training))
+
+            if self.training:
+                data = data.shuffle(100)
+
+            data = data.repeat(self.n_epochs)
+            data = data.batch(self.batch_size)
+            data = data.prefetch(self.batch_size * 8)
+            batch_x, batch_y = data.make_one_shot_iterator().get_next()
+            return batch_x, batch_y
+
+
 class BackboneNetwork:
     def __init__(self, name='resnet_v1_101', reg=1e-4, ckpt_path=None, output_stride=32):
         self.name = name
@@ -94,3 +200,25 @@ class BackboneNetwork:
             init_fn = assign_from_checkpoint_fn(self.ckpt_path, var_list, ignore_missing_vars=True)
         hooks = [RestoreHook(init_fn)]
         return conv5, hooks
+
+
+class VGGBackboneNetwork:
+    def __init__(self, reg=1e-4, ckpt_path=None):
+        self.reg = reg
+        self.ckpt_path = ckpt_path
+        self.vgg_fn = getattr(mu.load_module('nets.vgg'), 'vgg_16')
+        self.name = 'vgg_16'
+
+    def __call__(self, features, mode, params):
+        with slim.arg_scope(mu.vgg_arg_scope(weight_decay=self.reg)):
+            logits, _ = self.vgg_fn(features,
+                                    num_classes=params.n_classes + 1,
+                                    is_training=(mode == tf.estimator.ModeKeys.TRAIN),
+                                    spatial_squeeze=False,
+                                    fc_conv_padding='SAME')
+        init_fn = None
+        if self.ckpt_path is not None:
+            var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)
+            init_fn = assign_from_checkpoint_fn(self.ckpt_path, var_list, ignore_missing_vars=True)
+        hooks = [RestoreHook(init_fn)]
+        return logits, hooks

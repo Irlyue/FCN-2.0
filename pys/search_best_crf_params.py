3cc9628c382622de0ff0bee8cfd676befe026f76
Wenfeng <1554484645@qq.com>
2018-07-12 15:30:40 +0800

    search method changes

--- pys/search_best_crf_params.py
+++ pys/search_best_crf_params.py
@@ -1,44 +1,40 @@
 import argparse
-import numpy as np
 import my_utils as mu
 import tensorflow as tf
+import utils.image_utils as iu
 
 from post_processing.crf import crf_post_process
 from experiment import Experiment
-from models.model import SegInputFunction
-from scipy.misc import imresize
 from concurrent import futures
 from itertools import repeat
+from inputs import voc
+from tqdm import tqdm
 
 logger = mu.get_default_logger()
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_examples', type=int, default=100,
                     help='number of examples to do the grid search')
-parser.add_argument('--data', default='/tmp/voc2012_seg_val.tfrecord', type=str,
-                    help='tfrecord path for prediction')
-parser.add_argument('--n_workers', default=5, type=int,
-                    help='number of processes to use')
+parser.add_argument('--data', default='/tmp/voc2012_seg_val.tfrecord', type=str)
+parser.add_argument('--n_workers', default=4, type=int)
 
 
 def crf_config_gen():
     config_default = mu.load_json_config('configs/crf_config.json')
     # bilateral
-    compats = [3, 4, 5, 6]
-    sxys = [30, 40, 50, 60, 70, 80, 90, 100]
-    srgbs = [3, 4, 5, 6]
-    for compat in compats:
-        for sxy in sxys:
-            for srgb in srgbs:
-                new_params = {
-                    'bilateral': {
-                        'compat': compat,
-                        'sxy': sxy,
-                        'srgb': srgb
-                    }
-                }
-                config_new = config_default.copy()
-                config_new.update(new_params)
-                yield config_new
+    compats = range(3, 10)
+    sxys = range(50, 150, 10)
+    srgbs = range(3, 10)
+    for compat, sxy, srgb in ((x, y, z) for x in compats for y in sxys for z in srgbs):
+        new_params = {
+            'bilateral': {
+                'compat': compat,
+                'sxy': sxy,
+                'srgb': srgb
+            }
+        }
+        config_new = config_default.copy()
+        config_new.update(new_params)
+        yield config_new
 
 
 def grid_search_for_crf():
@@ -52,30 +48,6 @@ def grid_search_for_crf():
 
 
 def generate_pairs(tfrecord_path, n_examples):
-    class Resizer:
-        def __init__(self):
-            pass
-                                        
-        def __enter__(self):
-            self.sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
-            self.input_ph = tf.placeholder(tf.float32, shape=[None, None, None])
-            self.size_ph = tf.placeholder(tf.int32, shape=[2])
-            self.resized = tf.image.resize_images(self.input_ph, self.size_ph)
-            return self
-                                                                                                            
-        def __call__(self, x, size):
-            return self.sess.run(self.resized, feed_dict={self.input_ph: x, self.size_ph: size})
-
-        def __exit__(self, *args):
-            self.sess.close()
-    
-    def input_batch():
-        with tf.Graph().as_default():
-            input_fn = SegInputFunction(tfrecord_path, training=False, batch_size=1, n_epochs=1)
-            batch_images, batch_labels = input_fn()
-            with tf.Session(config=tf.ConfigProto(device_count={'GPU': 0})) as sess:
-                while True:
-                    yield sess.run([batch_images, batch_labels])
     logger.info('Generating %s pairs..', n_examples)
     parser = mu.get_default_parser()
     args = parser.parse_args('').__dict__.copy()
@@ -85,85 +57,26 @@ def generate_pairs(tfrecord_path, n_examples):
     config = mu.Config(args)
     exp = Experiment(config, training=False)
     results = []
-    with Resizer() as resizer:
-        for i, (inps, ends) in enumerate(zip(input_batch(), exp.predict())):
-            if i >= n_examples:
-                break
-            image = inps[0].astype('uint8')[0]
-            mask = np.squeeze(inps[1][0]).astype('int64')
-            probs = ends['up_probs']
-            probs = resizer(probs, mask.shape)
-            results.append((image, mask, probs))
-    logger.info('Done generation!')
+    meta = voc.load_meta_data('2012')
+    ids = voc.load_one_image_set(meta, 'val', task='Segmentation')
+    for idx, ends in tqdm(zip(ids[:n_examples], exp.predict()), total=n_examples):
+        image = iu.read_image(meta.img_path % idx)
+        mask = iu.read_mask(meta.cls_img_path % idx)
+        probs = ends['up_probs']
+        probs = iu.resize_maps(probs, mask.shape)
+        results.append((image, mask, probs))
     return results
 
 
-def eval_crf_config(data_in, config):
-    with mu.Timer() as timer:
-        with tf.Graph().as_default():
-            data = tf.data.Dataset.from_generator(lambda: crf_inference_config(data_in, config),
-                                                  output_types=(tf.int64, tf.int64),
-                                                  output_shapes=(tf.TensorShape([None, None]),
-                                                                 tf.TensorShape([None, None])))
-            batch_gt, batch_pred = data.repeat(1).prefetch(FLAGS.n_workers).batch(1).make_one_shot_iterator().get_next()
-            mIoU, update_op = mu.mean_iou(labels=batch_gt,
-                                          predictions=batch_pred,
-                                          num_classes=21)
-            with tf.Session(config=tf.ConfigProto(device_count={'GPU': 0})) as sess:
-                sess.run(tf.local_variables_initializer())
-                while True:
-                    try:
-                        sess.run(update_op)
-                    except tf.errors.OutOfRangeError:
-                        logger.info('Done!')
-                        break
-                miou = sess.run(mIoU)
-    print('******************************************', timer.eclipsed)
-    return miou, config
-
-
-def crf_inference_config(data, config):
-    """
-    Do CRF inference using given parameters on input data.
-
-    :param data: tuple[3], (image, mask, probability),
-        image: np.array, np.uint8, with shape(height, width, 3)
-        mask: np.array, np.int64, with shape(height, width)
-        probability: np.array, np.float32, with shape(height, width, n_classes=21)
-    :param config:
-    :return:
-    """
-    with futures.ProcessPoolExecutor(FLAGS.n_workers) as pool:
+def eval_crf_config(data, config):
+    with mu.MetricMIOU(n_classes=21) as metric, futures.ProcessPoolExecutor(FLAGS.n_workers) as pool:
         images = (item[0] for item in data)
         masks = (item[1] for item in data)
         probs = (item[2] for item in data)
-        mask_preds = pool.map(crf_post_process, images, probs, repeat(config))
-        yield from zip(masks, mask_preds)
-
-
-def eval_cnn(data_in):
-    def _gen():
-        for image, mask, prob in data_in:
-            mask_pred = np.argmax(prob, axis=-1)
-            yield mask, mask_pred
-    with tf.Graph().as_default():
-        data = tf.data.Dataset.from_generator(_gen, output_types=(tf.int64, tf.int64),
-                                              output_shapes=(tf.TensorShape([None, None]),
-                                                             tf.TensorShape([None, None])))
-        batch_gt, batch_pred = data.repeat(1).batch(1).make_one_shot_iterator().get_next()
-        mIoU, update_op = mu.mean_iou(labels=batch_gt,
-                                      predictions=batch_pred,
-                                      num_classes=21)
-        with tf.Session(config=tf.ConfigProto(device_count={'GPU': 0})) as sess:
-            sess.run(tf.local_variables_initializer())
-            while True:
-                try:
-                    sess.run(update_op)
-                except tf.errors.OutOfRangeError:
-                    logger.info('Done!')
-                    break
-            miou = sess.run(mIoU)
-    return miou
+        gen = zip((item[1] for item in data), pool.map(crf_post_process, images, masks, probs))
+        for mask, mask_pred in tqdm(gen, total=FLAGS.n_examples):
+            metric(mask, mask_pred)
+    return metric.result, config
 
 
 if __name__ == '__main__':

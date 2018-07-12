661ee00e53f703f681850e5265d8b23480472b8e
irlyue <1554484645@qq.com>
2018-07-12 10:32:27 +0800

    crf script changes

--- pys/eval_with_crf.py
+++ pys/eval_with_crf.py
@@ -1,4 +1,4 @@
-import argparse
+import os
 import numpy as np
 import my_utils as mu
 import tensorflow as tf
@@ -8,10 +8,13 @@ from models.model import SegInputFunction
 from concurrent import futures
 from post_processing.crf import crf_post_process
 from itertools import repeat
+from tqdm import tqdm
+from inputs import voc
 
 logger = mu.get_default_logger()
-parser = argparse.ArgumentParser()
-parser.add_argument('--data', default='/tmp/voc2012_seg_val.tfrecord', type=str)
+parser = mu.get_default_parser()
+parser.add_argument('--n_workers', default=8, type=int)
+parser.add_argument('--eval_func', default='cnn_output', type=str)
 
 
 def generate_pairs(tfrecord_path):
@@ -33,18 +36,17 @@ def generate_pairs(tfrecord_path):
             self.sess.close()
 
     def input_batch():
-        with tf.Graph().as_default():
+        g = tf.Graph()
+        with g.as_default():
             input_fn = SegInputFunction(tfrecord_path, training=False, batch_size=1, n_epochs=1)
             batch_images, batch_labels = input_fn()
-            with tf.Session(config=tf.ConfigProto(device_count={'GPU': 0})) as sess:
-                while True:
+        with tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}), graph=g) as sess:
+            while True:
+                try:
                     yield sess.run([batch_images, batch_labels])
-    parser = mu.get_default_parser()
-    args = parser.parse_args('').__dict__.copy()
-    args.update(image_size=(384, 384), aspp_rates=[12], keep_prob=1.0,
-                backbone_stride=8, kernel_size=3, data=tfrecord_path,
-                gpu_id=0)
-    config = mu.Config(args)
+                except tf.errors.OutOfRangeError:
+                    break
+    config = mu.Config(FLAGS.__dict__.copy())
     exp = Experiment(config, training=False)
     with Resizer() as resizer:
         for i, (inps, ends) in enumerate(zip(input_batch(), exp.predict())):
@@ -55,26 +57,8 @@ def generate_pairs(tfrecord_path):
             yield image, mask, probs
 
 
-class MetricMIOU:
-    def __init__(self):
-        pass
-
-    def __enter__(self):
-        self.sess = tf.Session(config=tf.ConfigProto(device_count={'GPU': 0}))
-        self.gt_ph = tf.placeholder(tf.int64, shape=[None, None])
-        self.pred_ph = tf.placeholder(tf.int64, shape=[None, None])
-        self.miou, self.update_op = mu.mean_iou(labels=self.gt_ph, predictions=self.pred_ph, num_classes=21)
-
-    def __call__(self, batch_gt, batch_pred):
-        self.sess.run(self.update_op, feed_dict={self.gt_ph: batch_gt, self.pred_ph: batch_pred})
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.result = self.sess.run(self.miou)
-        self.sess.close()
-
-
-def crf_inference(inputs, config):
-    image, mask, prob = inputs
+def crf_inference_one(item, config):
+    image, mask, prob = item
     mask_pred = crf_post_process(image, prob, config)
     return mask, mask_pred
 
@@ -85,22 +69,63 @@ def cnn_output(inputs, *args):
     return mask, mask_pred
 
 
+def show_class_iou(metric, timer):
+    print('********************************************')
+    print('{:<15}: {:.0f}s'.format('time', timer.eclipsed))
+    print('{:<15}: {:.4f}'.format('mIoU', metric.result))
+    class_names = ['background'] + voc.VOCMeta.classes
+    for name, iou in zip(class_names, metric.ious):
+        print('{:<15}: {:.4f}'.format(name, iou))
+    print('********************************************')
+
+
 def eval_crf(config):
-    with MetricMIOU as metric:
-        with futures.ProcessPoolExecutor as executor:
-            for mask, mask_pred in executor.map(crf_inference, generate_pairs(FLAGS.data), repeat(config)):
-                metric(mask, mask_pred)
-    print('************************', metric.result)
+    def batch_output(gen, batch_size):
+        batch_list = []
+        counter = 0
+        for item in gen:
+            batch_list.append(item)
+            counter += 1
+            if counter == batch_size:
+                yield batch_list
+                counter = 0
+                batch_list = []
+        if batch_list:
+            yield batch_list
+    eval_func = globals()[FLAGS.eval_func]
+    with futures.ProcessPoolExecutor(FLAGS.n_workers) as executor, mu.MetricMIOU(21) as metric, mu.Timer() as timer:
+        try:
+            for batches in batch_output(tqdm(generate_pairs(FLAGS.data)), FLAGS.n_workers*4):
+                for mask, mask_pred in executor.map(eval_func, batches, repeat(config)):
+                    metric(mask, mask_pred)
+        except Exception as e:
+            print(e)
+            pass
+    show_class_iou(metric, timer)
+
 
 
 def main():
+    # config = {
+        # 'unary': 'prob',
+        # 'n_steps': 5,
+        # 'bilateral': {
+            # 'compat': 9,
+            # 'sxy': 120,
+            # 'srgb': 4
+        # },
+        # 'gaussian': {
+            # 'sxy': 3,
+            # 'compat': 3
+        # }
+    # }
     config = {
         'unary': 'prob',
         'n_steps': 5,
         'bilateral': {
-            'compat': 6,
-            'sxy': 100,
-            'srgb': 6
+            'compat': 4,
+            'sxy': 121,
+            'srgb': 5
         },
         'gaussian': {
             'sxy': 3,
@@ -111,5 +136,7 @@ def main():
 
 
 if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
     FLAGS = parser.parse_args()
+    print(FLAGS)
     main()

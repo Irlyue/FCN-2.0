0e0af5e5b6ce866dfe2814181410989d3586e134
wenfeng <1554484645@qq.com>
2018-08-12 04:36:45 -0400

    a much efficient way to search CRF parameters

--- pys/search_best_crf_params.py
+++ pys/search_best_crf_params.py
@@ -15,17 +15,25 @@ logger = mu.get_default_logger()
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_examples', type=int, default=100,
                     help='number of examples to do the grid search')
-parser.add_argument('--data', default='/tmp/voc2012_seg_val.tfrecord', type=str)
+parser.add_argument('--data', default='/dev/shm/tmp/voc2012_seg_val.tfrecord', type=str)
 parser.add_argument('--n_workers', default=4, type=int)
 
 
 def crf_config_gen():
-    config_default = mu.load_json_config('configs/crf_config.json')
+    config_default = {
+        'unary': 'prob',
+        'n_steps': 5,
+        'gaussian': {
+            'compat': 3,
+            'sxy': 3
+        }
+
+    }
     # bilateral
     # compats = range(4, 5)
     # sxys = range(120, 130, 10)
     # srgbs = range(5, 6)
-    compats = range(3, 15)
+    compats = range(3, 10)
     sxys = range(30, 150, 5)
     srgbs = range(3, 10)
     for compat, sxy, srgb in ((x, y, z) for x in compats for y in sxys for z in srgbs):
@@ -46,10 +54,11 @@ def grid_search_for_crf():
     print('Done genreration!')
     time.sleep(10)
     best = {'mIoU': -1}
-    for mIoU, config in map(eval_crf_config, repeat(data), crf_config_gen()):
-        if mIoU > best['mIoU']:
-            best.update(config=config, mIoU=mIoU)
-            logger.info('Current Best mIoU=%.3f\nconfig=%s', best['mIoU'], best['config'])
+    with futures.ProcessPoolExecutor(FLAGS.n_workers) as pool:
+        for mIoU, config in tqdm(pool.map(eval_crf_config, repeat(data), crf_config_gen())):
+            if mIoU > best['mIoU']:
+                best.update(config=config, mIoU=mIoU)
+                logger.info('Current Best mIoU=%.3f\nconfig=%s', best['mIoU'], best['config'])
     logger.info('Best mIoU=%.3f\nconfig=%s', best['mIoU'], best['config'])
 
 
@@ -59,7 +68,7 @@ def generate_pairs(tfrecord_path, n_examples):
     args = parser.parse_args('').__dict__.copy()
     args.update(image_size=(384, 384), aspp_rates=[12], keep_prob=1.0,
                 backbone_stride=8, kernel_size=3, data=tfrecord_path,
-                gpu_id=0, model_dir='/tmp/fcn-3')
+                gpu_id=0, model_dir='/dev/shm/tmp/fcn')
     config = mu.Config(args)
     exp = Experiment(config, training=False)
     results = []
@@ -75,15 +84,15 @@ def generate_pairs(tfrecord_path, n_examples):
 
 
 def eval_crf_config(data, config):
-    with mu.MetricMIOU(n_classes=21) as metric, futures.ProcessPoolExecutor(FLAGS.n_workers) as pool:
+    with mu.MetricMIOU(n_classes=21) as metric:
         images = (item[0] for item in data)
         masks = (item[1] for item in data)
         probs = (item[2] for item in data)
         # for image, mask, prob in data:
             # mask_pred = prob.argmax(axis=-1)
             # metric(mask, mask_pred)
-        gen = zip(masks, pool.map(crf_post_process, images, probs, repeat(config)))
-        for mask, mask_pred in tqdm(gen, total=FLAGS.n_examples):
+        gen = zip(masks, map(crf_post_process, images, probs, repeat(config)))
+        for mask, mask_pred in gen:
             metric(mask, mask_pred)
     return metric.result, config
 
